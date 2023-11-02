0ffc4b78cf9543e1a12460074dac4adf55b4c8e9
abdulbasit <ba3568008@gmail.com>
2023-11-02 21:27:30 +0500

    made apis

---
+++ src/lib/drizzle.ts
@@ -0,0 +1,14 @@
+import { drizzle } from "drizzle-orm/vercel-postgres";
+import { sql } from "@vercel/postgres";
+import {InferModel} from "drizzle-orm"
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const cartTable = pgTable("basitcart", {
+  userid: varchar("userid", { length: 255 }).notNull(),
+  productid: varchar("productid", { length: 255 }).notNull(),
+  quantity: integer("quantity"),
+});
+
+export type typeOfCart = InferModel<typeof cartTable>
+
+export const db = drizzle(sql);
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,20 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import z from "zod";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const validateUserId = z.object({
+  userid: z.string()
+});
+export const validateDelete = z.object({
+  userid:z.string(),
+  productid:z.string()
+})
+export const validatePOST = z.object({
+  userid:z.string(),
+  productid:z.string(),
+  quantity:z.number()
+})
